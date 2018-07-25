c8fa0f66b9451467498e475769ad133989628ac6
Thomas Hanna <thomasjhanna@gmail.com>
2018-07-25 15:44:42 +0100

    Added babel webpack intergration

--- src/app.js
+++ src/app.js
@@ -1,13 +1,5 @@
-// import './utils.js';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-// import subtractRenamed, { square, add } from './utils.js'
-
-// console.log(square(5));
-// console.log(add(5,1));
-// console.log(subtractRenamed(5,2));
-
-import isSenior, {isAdult, canDrink} from './person.js'
-
-console.log(isAdult(17));
-console.log(canDrink(22));
-console.log(isSenior(66));
+const template = <p>THIS IS JSX FROM WEBPACK</p>
+ReactDOM.render(template, document.getElementById('app'));
