693adba697aaf57f7ee3b7fba9c2ffb2f3cd3af5
Thomas Hanna <thomasjhanna@gmail.com>
2018-07-25 15:10:21 +0100

    Added git import export examples and webpack

---
+++ src/person.js
@@ -0,0 +1,20 @@
+
+const isAdult = (age) => {
+  if (age >= 18) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+const canDrink = (age) => {
+  if (age >= 21) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+const isSenior = (age) => age >= 65
+
+export {isAdult, canDrink, isSenior as default };
---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+const path = require('path'); // require is a built in node function
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
+  }
+};
