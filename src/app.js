2e4d8ac4f90b97cfef9e57e73c2a754576097124
Thomas Hanna <thomasjhanna@gmail.com>
2018-07-26 11:22:05 +0100

    Added transform class properties

--- src/app.js
+++ src/app.js
@@ -1,118 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import AddOption from './components/AddOption.js';
-import Option from './components/Option.js';  
-import Header from './components/Header.js';
-import Action from './components/Action.js';
-
-
-class IndecisionApp extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
-    this.handlePick = this.handlePick.bind(this);
-    this.handleAddOption = this.handleAddOption.bind(this);
-    this.handleDeleteOption = this.handleDeleteOption.bind(this);
-    this.state = {
-      options: []
-    }
-  }
-  componentDidMount() {
-    try {
-      const json = localStorage.getItem('options');
-      const options = JSON.parse(json);
-
-      if (options) {
-        this.setState(() => ({ options: options }))
-      } 
-    } catch (e) {
-
-    }
-  }
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.options.length !== this.state.options.length) {
-      const json = JSON.stringify(this.state.options);
-      localStorage.setItem('options', json);
-    }
-  }
-  componentWillUnmount() {
-    console.log('componentWillUnmount');
-  }
-  handleDeleteOptions() {
-    this.setState(() => ({ options: [] }));
-  }
-  handlePick() {
-    const randomNumber = Math.floor(Math.random() * this.state.options.length)
-    const selectedOption = this.state.options[randomNumber];
-    alert(selectedOption);
-  }
-  handleAddOption(option) {
-    if (!option) {
-      return 'Enter valid value to add item';
-    } else if (this.state.options.indexOf(option) > -1) {
-      return 'This option already exists';
-    }
-    this.setState((prevState) => ({ options: prevState.options.concat([option]) }))
-  }
-  handleDeleteOption(optionToRemove) {
-    this.setState((prevState) => ({
-      options: prevState.options.filter((option) => {
-        return optionToRemove !== option;
-      })
-    }));
-  }
-  render() {
-    const title = 'Indecision';
-    const subtitle = 'Put your life in the hands of a computer';
-
-    return (
-      <div>
-        <Header subtitle={subtitle} />
-        <Action 
-          hasOptions={this.state.options.length > 0}
-          handlePick={this.handlePick}
-        />
-        <Options 
-          options={this.state.options} 
-          handleDeleteOptions={this.handleDeleteOptions}
-          handleDeleteOption={this.handleDeleteOption}
-        />
-        <AddOption 
-          handleAddOption={this.handleAddOption} 
-        />
-      </div>
-    );
-  }
-}
-
-
-Header.defaultProps = {
-  title: 'Indecision' // sets the default title, but other pages can overwrite it if they need to
-}
-
-
-
-const Options = (props) => {
-  return (
-    <div>
-      <button onClick={props.handleDeleteOptions}>Remove All</button>
-      {props.options.length === 0 && <p>Please add an option to get started</p>}
-      {
-        props.options.map((option) => (
-          <Option 
-            key={option}
-            optionText={option}
-            handleDeleteOption={props.handleDeleteOption}
-          /> 
-        ))
-      } 
-    </div>
-  )
-}
-
-
-
-
+import IndecisionApp from './components/IndecisionApp.js'
 
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+
+class OldSyntax {
+  constructor() {
+    this.name = 'Mike';
+    this.getGreeting = this.getGreeting.bind(this);
+  }
+  getGreeting() {
+    return `Hi. My name is ${this.name}.`;
+  }
+}
+
+const oldSyntax = new OldSyntax();
+const getGreeting = oldSyntax.getGreeting;
+console.log(getGreeting());
+
+// -------------
+
+class NewSyntax {
+  name = 'Jen';
+  getGreeting = () => {
+    return `Hi. My name is ${this.name}.`;
+  }
+}
+
+const newSyntax = new NewSyntax();
+const newGetGreeting = newSyntax.getGreeting;
+console.log(newGetGreeting());
